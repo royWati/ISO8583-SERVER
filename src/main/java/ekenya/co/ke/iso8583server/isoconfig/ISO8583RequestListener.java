5a88e32cacb502501feb60badc9b4812a368e87c
roywati <royywati@gmail.com>
2024-05-14 08:42:59 +0300

    add network management
    Signed-off-by: roywati <royywati@gmail.com>

--- src/main/java/ekenya/co/ke/iso8583server/isoconfig/ISO8583RequestListener.java
+++ src/main/java/ekenya/co/ke/iso8583server/isoconfig/ISO8583RequestListener.java
@@ -1,11 +1,15 @@
 package ekenya.co.ke.iso8583server.isoconfig;
 
+import ekenya.co.ke.iso8583server.network.NetworkManagement;
 import org.jpos.core.Configurable;
 import org.jpos.core.Configuration;
 import org.jpos.core.ConfigurationException;
+import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISORequestListener;
 import org.jpos.iso.ISOSource;
+
+import java.io.IOException;
 
 public class ISO8583RequestListener implements ISORequestListener, Configurable {
     @Override
@@ -21,8 +25,16 @@ public class ISO8583RequestListener implements ISORequestListener, Configurable 
 
         // this is the point where all the messages will be coming from
 
+        // check for network messages
+        try {
+            if (isoMsg.getMTI().equals("0800")) {
 
+                NetworkManagement.processNetworkCall(isoSource, isoMsg);
+            }
+        } catch (ISOException | IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        return false;
+        return true;
     }
 }
